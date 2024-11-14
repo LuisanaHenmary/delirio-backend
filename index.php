9a58a823beed261a7402d273875707e06d0ef256
LuisanaHenmary <luisanahenmaryp@gmail.com>
2024-11-14 17:11:38 -0400

    Add routes

--- index.php
+++ index.php
@@ -6,7 +6,11 @@
  */
 
 require_once "controllers/tables.php";
+require_once "controllers/user.php";
+require_once "routes.php";
 
 register_activation_hook(__FILE__, "wp_setup_api_tables");
 
 register_deactivation_hook(__FILE__, "wp_delete_api_tables");
+
+add_action('rest_api_init', 'wp_learn_register_routes');
