1e27e2ebfb625d4717630aeb39dd2a655a2cb9ae
LuisanaHenmary <luisanahenmaryp@gmail.com>
2024-11-17 11:09:48 -0400

    new response

--- controllers/user.php
+++ controllers/user.php
@@ -8,21 +8,22 @@ function wp_check_permission()
 function check_user_rol()
 {
     $user = wp_get_current_user();
+    $id_user = $user->id;
     $is_admin = in_array('administrator', (array) $user->roles);
     $is_employer = in_array('employer', (array) $user->roles);
     $is_company = in_array('company', (array) $user->roles);
 
     if ($is_admin) {
-        return new WP_REST_Response(array('role' => 'admin'), 200);
+        return new WP_REST_Response(array('role' => 'admin', 'id_user' => $id_user ), 200);
     }
 
     if ($is_employer) {
-        return new WP_REST_Response(array('role' => 'employer'), 200);
+        return new WP_REST_Response(array('role' => 'employer', 'id_user' => $id_user ), 200);
     }
 
     if ($is_company) {
-        return new WP_REST_Response(array('role' => 'company'), 200);
+        return new WP_REST_Response(array('role' => 'company', 'id_user' => $id_user ), 200);
     }
 
-    return new WP_REST_Response(array('role' => 'subscriptor'), 200);
+    return new WP_REST_Response(array('role' => 'subscriptor', 'id_user' => $id_user ), 200);
 }
